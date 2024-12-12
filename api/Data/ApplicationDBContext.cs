3138da3802f8cd49f5c1aca04b37340421d91402
Игорь Груздо <igor_gruzdo@bellintegrator.com>
2024-12-12 19:46:45 +0300

    done 21

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -1,11 +1,12 @@
 using api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext<AppUser>
 {
     public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
     // Определите DbSet для каждой сущности
-    public DbSet<Stock> Stocks { get; set; }
+    public DbSet<Stock> Stocks { get; set; }  
     public DbSet<Comment> Comments { get; set; }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5fdb0ed05482e21853941cc589386d57ea6ea17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+600b0179446a820916e8dfd2175c2c8f5d1c34d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
