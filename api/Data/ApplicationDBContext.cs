ec11b71012d57b8b7b2314fc21e418b272b0a20b
Игорь Груздо <igor_gruzdo@bellintegrator.com>
2024-12-14 00:45:42 +0300

    done 22

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,4 +10,23 @@ public class ApplicationDbContext : IdentityDbContext<AppUser>
     // Определите DbSet для каждой сущности
     public DbSet<Stock> Stocks { get; set; }  
     public DbSet<Comment> Comments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        List<IdentityRole> identityRoles = new List<IdentityRole>
+        {
+            new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN",
+            },
+            new IdentityRole
+            {
+                Name = "User",
+                NormalizedName = "USER",
+            },
+        };
+        modelBuilder.Entity<IdentityRole>().HasData(identityRoles); 
+    }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+600b0179446a820916e8dfd2175c2c8f5d1c34d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3138da3802f8cd49f5c1aca04b37340421d91402")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
